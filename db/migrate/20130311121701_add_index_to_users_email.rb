c0e444685f35e4a1dc59519045af43135af94565
kishansoni <kishansoni@heraldsolutions.in>
2013-03-12 18:16:00 +0530

    Finish sign in

--- db/migrate/20130311121701_add_index_to_users_email.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToUsersEmail < ActiveRecord::Migration
-  def change
-    add_index :users, :email, :unique => true
-    #remove_index(:users, :name => 'index_users_on_email')
-  end
-end
