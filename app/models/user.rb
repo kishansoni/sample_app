5f72a3ee1d44a073a138f11e49d160fcb5b59f7e
kishansoni <kishansoni@heraldsolutions.in>
2013-03-14 15:14:09 +0530

    Finish user edit update, index, and destroy action

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,9 +24,8 @@ class User < ActiveRecord::Base
   validates :password, :presence => 'true', :length => { :minimum => 6 }
   validates :password_confirmation, :presence => 'true'
 
-  private
-
   def create_remember_token
-    self.remember_token = SecureRandom.urlsafe_base64
+    self.remember_token = SecureRandom.base64.tr("+/", "-_")
   end
+
 end
